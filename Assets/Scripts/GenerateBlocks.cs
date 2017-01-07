f56def466a9aa3a59250a90017d81d49b7f334de
Andrew McPherson <andrewmcp333@gmail.com>
2017-01-07 14:06:16 -0800

    Read the positions of the blocks from JSON.

--- Assets/Scripts/GenerateBlocks.cs
+++ Assets/Scripts/GenerateBlocks.cs
@@ -4,12 +4,21 @@ using SimpleJSON;
 
 public class GenerateBlocks : MonoBehaviour {
 
+	public GameObject obj;
+
 	void Start() {
 		TextAsset asset = Resources.Load("Map") as TextAsset;
 		var json = JSON.Parse(asset.text);
 
-		string message = json["message"];
-		Debug.Log(message);
+		JSONNode Blocks = json["blocks"];
+		foreach(JSONNode node in Blocks.Children) {
+			float x = node["position"]["x"].AsFloat;
+			float y = node["position"]["y"].AsFloat;
+			float z = node["position"]["z"].AsFloat;
+			Vector3 position = new Vector3(x, y, z);
+
+			Instantiate(obj, position, Quaternion.identity, transform);
+		}
 	}
 
 	void Update() {
